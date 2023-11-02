6f6c97e4976c112e71946faed8a60f7c7dbf015b
foziljonov7 <abdulvosidfoziljonov55@gmail.com>
2023-11-02 15:39:55 +0500

    Create Models and DbContext

---
+++ API/Entities/Human.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Entities
+{
+    public class Human
+    {
+        public Guid Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public int Age { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Location { get; set; }
+        public int CategoryId { get; set; }
+        public int TicketId { get; set; }
+        public virtual Category Category { get; set; }
+        public virtual Ticket Ticket { get; set; }
+    }
+}
---
+++ API/Entities/Ticket.cs
@@ -0,0 +1,10 @@
+﻿namespace API.Entities
+{
+    public class Ticket
+    {
+        public int Id { get; set; }
+        public string Flight { get; set; }
+        public double Price { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
