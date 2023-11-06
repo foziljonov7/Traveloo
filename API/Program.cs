da54ec42d3007509229b1d12d5de8bef5c8ba9e7
foziljonov7 <abdulvosidfoziljonov55@gmail.com>
2023-11-06 15:20:42 +0500

    create Identity model

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,8 @@
 using API.Data;
+using API.Entities.User;
 using API.Services;
+using Identity.Data;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +13,12 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(options
     => options.UseNpgsql(connectionString));
 
+builder.Services.AddDbContext<ApplicationDbContext>(options
+    => options.UseNpgsql(connectionString));
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
 //Services
 builder.Services.AddScoped<IHumanService, HumanService>();
 builder.Services.AddScoped<ITicketService, TicketService>();
---
+++ Identity/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using API.Entities.User;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            :base(options) { }
+    }
+}
